58bf82d59b2b3b4bfcdbd42ac0563c7c058787aa
John Mack <mackspace@gmail.com>
2021-06-28 10:41:53 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const INITIAL_STATE = {
+    narrative: "",
+  };
+
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  //handles changes to controlled form
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <textarea
+        rows="4"
+        name="narrative"
+        id="narrative"
+        value={formData.narrative}
+        onChange={handleChange}
+        required
+        placeholder="e.g. This amazing hike takes you by two different waterfalls but the trailhead can be hard to find. The parking area is..."
+      />
     </div>
   );
 }
